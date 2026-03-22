8ce4533493d53eba2c627126d41ac585d5fb199c
iOmega8561 <giusepperocco38@gmail.com>
2026-03-22 20:53:58 +0100

    Created a standard structure for OpenAI API requests - Basically encapsulates the details of a request. Also offers a convenience "perform(_)" method that sets the Foundation URL request up and delivers the result (or throws the error).

---
+++ OpenChat/Service/OpenAIRequest.swift
@@ -0,0 +1,41 @@
+//
+//  OpenAIRequest.swift
+//  OpenChat
+//
+//  Created by Giuseppe Rocco on 22/03/26.
+//
+
+import Foundation
+
+struct OpenAIRequest<RequestType: Encodable>: Sendable {
+    enum Method: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum ContentType: String {
+        case json = "application/json"
+    }
+    
+    let body: RequestType?
+    let contentType: ContentType
+    let method: Method
+    let path: String
+    
+    func perform(
+        endpoint: EndpointConfiguration,
+        session: URLSession
+    ) async throws -> (Data, URLResponse) {
+        let url = endpoint.baseURL.appendingPathComponent(self.path)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = self.method.rawValue
+        urlRequest.addValue(self.contentType.rawValue, forHTTPHeaderField: "Content-Type")
+        
+        if self.body != nil {
+            urlRequest.httpBody = try JSONEncoder().encode(self.body)
+        }
+        
+        return try await session.data(for: urlRequest)
+    }
+}
