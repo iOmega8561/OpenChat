9316175edda24324a6f13a11fc3c9278d78a6497
iOmega8561 <giusepperocco38@gmail.com>
2026-03-22 14:52:35 +0100

    Initial commit - blank xcode project

---
+++ OpenChat/OpenChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenChatApp.swift
+//  OpenChat
+//
+//  Created by Giuseppe Rocco on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
